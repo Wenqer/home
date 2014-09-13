0209534efc32a724ac2eda5f9258ea69b4fd85a5
yvoloshchuk <yaroslav.voloshchuk@grammarly.com>
2014-09-13 18:20:35 +0300

    volume test

--- volume/index.js
+++ volume/index.js
@@ -1,7 +1,17 @@
-function index(req,res) {
-  res.json({'hello':'volume'})
+var exec = require('child_process').exec
+
+function index(req, res) {
+  var vol = req.params.value
+  exec('cat ' + vol, function(err, stdout, stderr) {
+    res.json({
+      value: vol,
+      err: err,
+      stdout: stdout,
+      stderr: stderr
+    })
+  })
 }
 
 module.exports = function setup (app) {
-  app.get('/volume', index)
+  app.get('/volume/:value', index)
 }
