fe13b778f0814a799d804602c74e61d89f2cae9e
Yaroslav <wenqeer@gmail.com>
2014-09-14 18:56:46 +0300

    volume module

--- volume/index.js
+++ volume/index.js
@@ -2,7 +2,7 @@ var exec = require('child_process').exec
 
 function index(req, res) {
   var vol = req.params.value
-  exec('cat ' + vol, function(err, stdout, stderr) {
+  exec('volume ' + vol, function(err, stdout, stderr) {
     res.json({
       value: vol,
       err: err,
